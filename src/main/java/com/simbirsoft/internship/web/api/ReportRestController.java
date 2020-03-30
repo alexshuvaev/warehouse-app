988ba842f8568e1d6daf3a67ae25ae82d461ee7e
alexshuvaev <alexshuvaevdev@gmail.com>
2020-03-30 09:56:04 +0300

    Add javadoc to ReportRestController

--- src/main/java/com/simbirsoft/internship/web/api/ReportRestController.java
+++ src/main/java/com/simbirsoft/internship/web/api/ReportRestController.java
@@ -4,6 +4,7 @@ import com.simbirsoft.internship.dto.report.RevenueReport;
 import com.simbirsoft.internship.dto.report.StoreSalesReport;
 import com.simbirsoft.internship.dto.report.WriteOffReport;
 import com.simbirsoft.internship.service.ReportService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -17,36 +18,63 @@ public class ReportRestController {
     private ReportService reportService;
 
     @Autowired
+
     public ReportRestController(ReportService reportService) {
         this.reportService = reportService;
     }
 
     /**
+     * Main KPI report.
+     * @see ReportService#getRevenueReport
      *
-     *
-     * @param startDate
-     * @param endDate
-     * @return
+     * @param a_startDate generate report from startDate. If null, report will be generated from first record.
+     * @param b_endDate generate report up to endDate. If null, report will be generated from startDate and up to date.
+     *                  If both null, report will be on current date.
+     * @return report or NotFoundException if no data in DB.
      */
+    @ApiOperation(value = "Get main KPI report", notes = "Input startDate and endDate to generate a report. " +
+            "If startDate and endDate not provided, will be generated report on today's date.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "")
-    public RevenueReport getRevenueReport(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                          @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-        return reportService.getRevenueReport(startDate, endDate);
+    public RevenueReport getRevenueReport(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate a_startDate,
+                                          @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate b_endDate) {
+        return reportService.getRevenueReport(a_startDate, b_endDate);
     }
 
+    /**
+     * Store sales report.
+     * @see ReportService#getStoreSalesReport 
+     *
+     * @param a_startDate generate report from startDate. If null, report will be generated from first record.
+     * @param b_endDate generate report up to endDate. If null, report will be generated from startDate and up to date.
+     *                  If both null, report will be on current date.
+     * @return report or NotFoundException if no data in DB.
+     */
+    @ApiOperation(value = "Get sales report on single store", notes = "Input startDate and endDate to generate a report. " +
+            "If startDate and endDate not provided, will be generated report on today's date.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/store/{storeId}")
     public StoreSalesReport getStoreSalesReport(@PathVariable int storeId,
-                                                @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                                @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-        return reportService.getStoreSalesReport(storeId, startDate, endDate);
+                                                @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate a_startDate,
+                                                @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate b_endDate) {
+        return reportService.getStoreSalesReport(storeId, a_startDate, b_endDate);
     }
 
+    /**
+     * Write-off report.
+     * @see ReportService#getWriteOffReport 
+     *
+     * @param a_startDate generate report from startDate. If null, report will be generated from first record.
+     * @param b_endDate generate report up to endDate. If null, report will be generated from startDate and up to date.
+     *                  If both null, report will be on current date.
+     * @return report or NotFoundException if no data in DB.
+     */
+    @ApiOperation(value = "Get write-off report", notes = "Input startDate and endDate to generate a report. " +
+            "If startDate and endDate not provided, will be generated report on today's date.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/writeoff")
-    public WriteOffReport getWriteOffReport(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-        return reportService.getWriteOffReport(startDate, endDate);
+    public WriteOffReport getWriteOffReport(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate a_startDate,
+                                            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate b_endDate) {
+        return reportService.getWriteOffReport(a_startDate, b_endDate);
     }
 }
